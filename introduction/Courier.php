bd8d7940692959bd1fa02383200099896931c6f7
Mamithi <19lawrence93@gmail.com>
2017-12-12 20:49:20 +0300

    First commit

---
+++ introduction/Courier.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mamithi
+ * Date: 12/12/17
+ * Time: 8:45 PM
+ */
+
+class courier
+{
+    public $name;
+    public $home_country;
+
+    public function __construct()
+    {
+
+    }
+}
